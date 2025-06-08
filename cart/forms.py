a61e6370e643039152779189bca59fb82f546b21
redlikeroses <devmaksim2006@gmail.com>
2025-06-08 15:07:53 +0300

    Переход на PostgreSQL

--- cart/forms.py
+++ cart/forms.py
@@ -1,6 +1,7 @@
 
 
 from django import forms
+from django.template.context_processors import request
 
 from main.models import Color, Size, MetalType, ProductVariation
 
@@ -9,11 +10,6 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 class CartAddProductForm(forms.Form):
 
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,coerce=int)
-    color = forms.ModelChoiceField(queryset = Color.objects.none(),required=False)
-    size = forms.ModelChoiceField(queryset=Size.objects.none(), required=False)
-    metal_type = forms.ModelChoiceField(queryset=MetalType.objects.none(), required=False)
-
-
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
 
 
@@ -25,15 +21,44 @@ class CartAddProductForm(forms.Form):
             variations = ProductVariation.objects.filter(product=product)
 
             if variations.exists():
-                self.fields['color'].queryset = Color.objects.filter(
+                # Цвета
+                colors = Color.objects.filter(
                     id__in=variations.exclude(color=None).values_list('color', flat=True).distinct()
                 )
-                self.fields['size'].queryset = Size.objects.filter(
-                    id__in=variations.exclude(size=None).values_list('size',flat=True).distinct()
+                if colors.exists():  # Проверяем, есть ли результаты
+                    self.fields['color'] = forms.ModelChoiceField(
+                        queryset=colors,
+                        required=False,
+                        label='Цвет'
+                    )
+
+                # Размеры
+                sizes = Size.objects.filter(
+                    id__in=variations.exclude(size=None).values_list('size', flat=True).distinct()
                 )
-                self.fields['metal_type'].queryset = MetalType.objects.filter(
-                    id__in=variations.exclude(metal_type=None).values_list('metal_type', flat=True).distinct()
+                if sizes.exists():  # Проверяем, есть ли результаты
+                    self.fields['size'] = forms.ModelChoiceField(
+                        queryset=sizes,
+                        required=False,
+                    )
+
+                # Типы металлов
+                metals = MetalType.objects.filter(
+                    id__in=variations.values_list('metal_type', flat=True).distinct()
                 )
+                if metals.exists():  # Проверяем, есть ли результаты
+                    self.fields['metal_type'] = forms.ModelChoiceField(
+                        queryset=metals,
+                        required=True,
+                        label='Metal Type'
+                    )
+
+                # self.fields['size'].queryset = Size.objects.filter(
+                #     id__in=variations.exclude(size=None).values_list('size',flat=True).distinct()
+                # )
+                # self.fields['metal_type'].queryset = MetalType.objects.filter(
+                #     id__in=variations.exclude(metal_type=None).values_list('metal_type', flat=True).distinct()
+                # )
 
 
 
--- redlikeroses/settings.py
+++ redlikeroses/settings.py
@@ -84,8 +84,13 @@ WSGI_APPLICATION = 'redlikeroses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'redlikeroses',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
